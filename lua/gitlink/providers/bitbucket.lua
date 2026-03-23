9722edf55af7e6c7f7c6e1186913adc24fe2bc9d
Flavio Milinanni <103764622+FlavioMili@users.noreply.github.com>
2026-03-23 10:39:21 +0100

    Refactor Bitbucket URL building logic

--- lua/gitlink/providers/bitbucket.lua
+++ lua/gitlink/providers/bitbucket.lua
@@ -1,25 +1,26 @@
 local M = {}
+
+local function owner_segment(owner)
+  if owner:sub(1, 1) == "~" then
+    return "users/" .. owner:sub(2)
+  else
+    return "projects/" .. owner:upper()
+  end
+end
+
 function M.build(info, branch, path, l1, l2)
-  local is_cloud = info.host == "bitbucket.org"
   local url
 
-  if is_cloud then
+  if info.host == "bitbucket.org" then
     url = string.format(
       "https://bitbucket.org/%s/%s/src/%s/%s",
       info.owner, info.repo, branch, path
     )
   else
-    local owner_segment
-    if info.owner:sub(1, 1) == "~" then
-      owner_segment = "users/" .. info.owner:sub(2)
-    else
-      owner_segment = "projects/" .. info.owner:upper()
-    end
-
     url = string.format(
       "https://%s/%s/repos/%s/browse/%s?at=refs%%2Fheads%%2F%s",
       info.host,
-      owner_segment,
+      owner_segment(info.owner),
       info.repo,
       path,
       branch
@@ -28,7 +29,7 @@ function M.build(info, branch, path, l1, l2)
 
   if not l1 then return url end
 
-  if is_cloud then
+  if info.host == "bitbucket.org" then
     if l1 == l2 then return url .. "#lines-" .. l1 end
     return url .. "#lines-" .. l1 .. ":" .. l2
   else
@@ -36,4 +37,5 @@ function M.build(info, branch, path, l1, l2)
     return url .. "#" .. l1 .. "-" .. l2
   end
 end
+
 return M
